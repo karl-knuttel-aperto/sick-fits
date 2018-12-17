63cca3977da7b2d0a4a43cdc3853f85ad239f4c0
Knuttel, Karl <karl.knuttel@aperto.com>
2018-12-17 16:48:39 +0100

    Small changes to formatting

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,8 +3,10 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
-    const sheet     = new ServerStyleSheet();
-    const page      = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const sheet = new ServerStyleSheet();
+    const page  = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
